9cd3a51dbb2112846172b34a60d8b0a607f2f9e7
Marcílio Freitas <marcilio@beltsistemas.com.br>
2024-10-28 08:00:17 -0300

    Início da implementação do html semântico e ajustes no servidor nodemon.

--- js/server.js
+++ js/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 const port = 8881;
-const www = './../';
+const www = './';
 
 app.use(express.static(www));
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
